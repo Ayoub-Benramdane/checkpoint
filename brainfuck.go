c4f1bd3f016b1beccc7e78059834186016083b51
Ayoub Benramdane <145620173+Ayoub-Benramdane@users.noreply.github.com>
2024-08-28 01:57:25 +0100

    new

--- brainfuck.go
+++ brainfuck.go
@@ -9,11 +9,11 @@ func main() {
 	if len(os.Args) != 2 || len(os.Args[1]) > 4096 {
 		return
 	}
-	var indice, m39ofa int
-	str := ""
+	var indice int
+	var m39ofat []int
 	table := [2048]byte{}
-	for i, c := range os.Args[1] {
-		switch c {
+	for i := 0; i < len(os.Args[1]); i++ {
+		switch os.Args[1][i] {
 		case '+':
 			{
 				table[indice]++
@@ -37,20 +37,21 @@ func main() {
 						i++
 					}
 				} else {
-					m39ofa = i
+					m39ofat = append(m39ofat, i)
 				}
 			}
 		case ']':
 			{
 				if table[indice] != 0 {
-					i = m39ofa
+					i = m39ofat[len(m39ofat)-1]
+				} else {
+					m39ofat = m39ofat[:len(m39ofat)-1]
 				}
 			}
 		case '.':
 			{
-				str += string(table[indice])
+				fmt.Print(string(table[indice]))
 			}
 		}
 	}
-	fmt.Println(str)
 }
